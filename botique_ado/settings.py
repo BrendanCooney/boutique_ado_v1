87e0812e259cb1c730abab0eef54144603114cc0
BrendanCooney <advicast@gmail.com>
2023-10-30 22:28:27 +0000

    working on checkout form

--- botique_ado/settings.py
+++ botique_ado/settings.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    
+
+    #Other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -67,6 +70,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'botique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -84,6 +89,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
         },
     },
 ]
