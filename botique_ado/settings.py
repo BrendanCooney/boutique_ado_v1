ed2f883743f83f51820e70dc647529b126a28837
BrendanCooney <advicast@gmail.com>
2023-11-09 22:04:03 +0000

    database setup

--- botique_ado/settings.py
+++ botique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -131,8 +132,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   }
 }
+
+
 
 
 # Password validation
